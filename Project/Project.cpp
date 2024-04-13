4b270e76f3d08fad3b94369b73f01de823aaa041
Bhawanjeet Kaur <bhawanjeetgill@gmail.com>
2024-04-13 01:44:27 -0400

    Prgram completed

---
+++ Project/Project.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    char choice[10];
+    int error = -1;
+    printf("Please make menu choice: ");
+    if (fgets(choice, sizeof(choice), stdin) != NULL) {
+        // Invalid input (not a number)
+        int number = atoi(choice);
+        printf("%d", number);
+        return 0;
+        //printf("%d", error);
+        //return 1;
+    }
+    else
+    {
+        printf("%d", error);
+        return 1;
+    }
+}
