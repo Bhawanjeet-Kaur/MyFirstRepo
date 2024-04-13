f9f21739ecb7f6dd72c09bd85d481687707fffe0
Bhawanjeet Kaur <bhawanjeetgill@gmail.com>
2024-04-13 14:08:42 -0400

    Added a new line

--- Project/Project.cpp
+++ Project/Project.cpp
@@ -4,18 +4,23 @@
 int main(void) {
     char choice[10];
     int error = -1;
-    printf("Please make menu choice: ");
-    if (fgets(choice, sizeof(choice), stdin) != NULL) {
-        // Invalid input (not a number)
-        int number = atoi(choice);
-        printf("%d", number);
-        return 0;
-        //printf("%d", error);
-        //return 1;
-    }
-    else
+    for (int count = 0;count <= 5;count++)
     {
-        printf("%d", error);
-        return 1;
+        printf("Please make menu choice: ");
+        if (fgets(choice, sizeof(choice), stdin) != NULL) {
+            // Invalid input (not a number)
+            int number = atoi(choice);
+            printf("%d", number);
+            //printf("%d", error);
+            //return 1;
+        }
+        else
+        {
+            printf("%d\n",error);
+            printf("%d", error);
+            return 1;
+        }
     }
+    return 0;
 }
+//End of the code
